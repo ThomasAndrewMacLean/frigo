8e3b0d05bc9872f79fd1e4d32d1c23c19e3e877e
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-11-11 10:44:32 +0100

    init project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+var fs = require("fs"); // this engine requires the fs module
+app.engine("ntl", function (filePath, options, callback) {
+  // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(err);
+    // this is an extremely simple template engine
+    var rendered = content
+      .toString()
+      .replace("#title#", "<title>" + options.title + "</title>")
+      .replace("#message#", "<h1>" + options.message + "</h1>");
+    return callback(null, rendered);
+  });
+});
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "ntl"); // register the template engine
+
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
